fb9e7285c1fea5f28968ceea9812d94e328a68c9
shahin-hossain-dev <shahin.hossain.dev@gmail.com>
2024-02-02 19:03:00 +0600

    20-1 What Are Function And Function Syntax

---
+++ function.js
@@ -0,0 +1,55 @@
+/**
+ * 
+ * Function Declaration
+ */
+/* 
+// function লিখতে হলে প্রথমেই function কীওয়ার্ড লিখতে হবে।
+//function এর একটি নাম থাকবে।
+// function এর নামের পরে parentheses() ব্যবহার করতে হবে।
+// parentheses() এর পরে curly brackets{} ব্যবহার করতে হবে এবং curly brackets{} এর ভিতরে কোড  লিখতে হবে।
+// fuction এর ভিতরে এক বা একাধিক কাজ করা যায়।
+//fuction invocation /call => ফাংশনটি কল করা হলে, এর ভিতরের কোডটি কাযকর হবে।
+//একটি ফাংশন কোড একবার লিখলেই হয়। তারপর যতবার ইচ্ছা ব্যবহার করা যায়।
+
+//loop and function এর মধ্যে পার্থ্ক্য হল, লুপ একবারে অনেক গুলো এলিমেনকে একবারে দেখাবে।
+//কিন্তু অনেক গুলো function কল করলেও করে পরে পরে অন্য কাজ করা যাবে।
+
+*/
+/* 
+Function structure,
+1. function keyword
+2. function name
+3. parenthses 
+4. function body with curly-basis {}
+5. function call => funcName()
+
+function funcName (){
+    
+}
+ */
+
+function switchOffFan() {
+    console.log('Uthe darao')
+    console.log('Switch er kase jao');
+    console.log('switch a click koro')
+}
+
+// function call
+
+
+switchOffFan()
+
+//function Declaration ::::::::::::::::
+
+function startFan() {
+    console.log('Stand up');
+    console.log('walk towards the switch');
+    console.log('press the switch');
+}
+
+//call the function ::::::::
+
+startFan();
+console.log('I watching JS tutorial')
+startFan();
+startFan();
